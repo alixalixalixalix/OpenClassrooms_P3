23661d0bef7666cddf5abc979742691b8c312c19
Alix Bocquier <alixbocqui@gmail.com>
2024-10-18 09:10:25 +0200

    Filtre + début login

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -8,6 +8,91 @@ fetch('http://localhost:5678/api/works')
 const reponse = await fetch("http://localhost:5678/api/works");
 const works = await reponse.json();
 
-const article = works[0]
+function generatorWorks(works){
+    for(let i = 0 ; i < works.length ; i++){
+        let worksImg = works[i].imageUrl
+        let worksTitle = works[i].title
+        
+        let figureBalise = document.createElement("figure");
+        let imageBalise = document.createElement("img");
+        let figcaptionBalise = document.createElement("figcaption");
 
-console.log(article.title)
+        let gallery = document.querySelector('.gallery')
+        gallery.appendChild(figureBalise) // on met la balise figure dans la div gallery
+        figureBalise.appendChild(imageBalise) // on met la balise img dans la figure
+        figureBalise.appendChild(figcaptionBalise) // idem avec figcaption
+
+        imageBalise.src = worksImg
+        figcaptionBalise.innerText = worksTitle
+    }
+}
+
+let buttonTous = document.querySelector('.filters-tous')
+let buttonObjets = document.querySelector('.filters-objets')
+let buttonAppartements = document.querySelector('.filters-appartements')
+let buttonHotels = document.querySelector('.filters-hotels')
+
+buttonTous.addEventListener("click", function(){
+    let filtersTous = works.filter(function (work){
+        return work.category.id >= 0
+    })
+    document.querySelector(".gallery").innerHTML = '';
+    generatorWorks(filtersTous)
+})
+
+buttonObjets.addEventListener("click", function(){
+    let filtersObjets = works.filter(function (work){
+        return work.category.name == "Objets"
+    })
+    document.querySelector(".gallery").innerHTML = '';
+    generatorWorks(filtersObjets)
+})
+
+buttonAppartements.addEventListener("click", function(){
+    let filtersAppartements = works.filter(function (work){
+        return work.category.name == "Appartements"
+    })
+    document.querySelector(".gallery").innerHTML = '';
+    generatorWorks(filtersAppartements)
+})
+
+buttonHotels.addEventListener("click", function(){
+    let filtersHotels = works.filter(function (work){
+        return work.category.name == "Hotels & restaurants"
+    })
+    document.querySelector(".gallery").innerHTML = '';
+    generatorWorks(filtersHotels)
+})
+
+generatorWorks(works)
+
+
+
+
+/*
+TENTATIVE DE FILTER ÉCHOUÉE
+buttonObjets.addEventListener("click", function(){
+    for(let i = 0 ; i < works.length ; i++){
+        let x = works[i].category.name;
+        let queObjets = works.filter(function(works){
+            return works.category.name = "Objets"
+        })
+        console.log(queObjets)
+    }
+})
+*/
+
+
+
+/*
+let filtersButtons = document.querySelectorAll(".filters button")
+
+fonction(le bouton objet){
+    if le bouton est inactif {
+        le bouton on lui retire la class inactive 
+        le bouton on lui met la class active
+        les autres boutons sont inactif
+    }
+}
+je suis bloqué car je n'arrive pas à mettre dans la fonction un paramètre non défini (en l'occurence l'un des 4 boutons)
+*/
